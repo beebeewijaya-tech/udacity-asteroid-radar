c70ae6b522de74db58cf19b878c8d8b4e2e7534b
Bee Bee Wijaya <beebeewijaya@gmail.com>
2021-04-02 19:11:25 +0700

    FINISH: Task

--- starter/app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
@@ -1,21 +1,22 @@
 package com.udacity.asteroidradar.api
 
-import com.udacity.asteroidradar.Asteroid
+import android.util.Log
 import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.database.DatabaseAsteroid
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<Asteroid> {
+fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<DatabaseAsteroid> {
     val nearEarthObjectsJson = jsonResult.getJSONObject("near_earth_objects")
 
-    val asteroidList = ArrayList<Asteroid>()
+    val asteroidList = ArrayList<DatabaseAsteroid>()
 
     val nextSevenDaysFormattedDates = getNextSevenDaysFormattedDates()
     for (formattedDate in nextSevenDaysFormattedDates) {
-        val dateAsteroidJsonArray = nearEarthObjectsJson.getJSONArray(formattedDate)
 
+        val dateAsteroidJsonArray = nearEarthObjectsJson.getJSONArray(formattedDate)
         for (i in 0 until dateAsteroidJsonArray.length()) {
             val asteroidJson = dateAsteroidJsonArray.getJSONObject(i)
             val id = asteroidJson.getLong("id")
@@ -33,7 +34,7 @@ fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<Asteroid> {
             val isPotentiallyHazardous = asteroidJson
                 .getBoolean("is_potentially_hazardous_asteroid")
 
-            val asteroid = Asteroid(id, codename, formattedDate, absoluteMagnitude,
+            val asteroid = DatabaseAsteroid(id, codename, formattedDate, absoluteMagnitude,
                 estimatedDiameter, relativeVelocity, distanceFromEarth, isPotentiallyHazardous)
             asteroidList.add(asteroid)
         }
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,36 +1,63 @@
 package com.udacity.asteroidradar.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.squareup.picasso.Picasso
+import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentMainBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+        binding = FragmentMainBinding.inflate(inflater)
 
+        val viewModelFactory = MainViewModelFactory(requireActivity().application)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        setHasOptionsMenu(true)
+        val adapter = MainFragmentAdapter(AsteroidClickListener { asteroid ->
+            findNavController().navigate(MainFragmentDirections.actionShowDetail(asteroid))
+        })
+        binding.asteroidRecycler.adapter = adapter
+
+        binding.statusLoadingWheel.visibility = View.VISIBLE
+        binding.asteroidRecycler.visibility = View.GONE
+
+        viewModel.asteroidList.observe(viewLifecycleOwner, Observer { asteroids ->
+            asteroids?.let {
+                adapter.submitList(asteroids)
+                binding.statusLoadingWheel.visibility = View.GONE
+                binding.asteroidRecycler.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel.pictureOfDay.observe(viewLifecycleOwner, Observer { picture ->
+            picture?.let {
+                if (picture.mediaType == "image") {
+                    setPictureOfDay(picture)
+                }
+            }
+        })
 
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.main_overflow_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return true
+    private fun setPictureOfDay(pictureOfDay: PictureOfDay) {
+        Picasso.with(binding.activityMainImageOfTheDay.context)
+            .load(pictureOfDay.url)
+            .placeholder(R.drawable.loading_img)
+            .error(R.drawable.ic_broken_image)
+            .into(binding.activityMainImageOfTheDay)
     }
 }
